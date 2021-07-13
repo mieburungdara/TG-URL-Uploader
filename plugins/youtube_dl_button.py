1b9cf7bbeda04c1e3e9e09e8340a6547857f2681
mieburungdara <rereitureza@gmail.com>
2021-07-13 11:48:46 +0800

    2

--- plugins/youtube_dl_button.py
+++ plugins/youtube_dl_button.py
@@ -20,7 +20,7 @@ from datetime import datetime
 if bool(os.environ.get("WEBHOOK", False)):
     from sample_config import Config
 else:
-    from config import Config
+    from sample_config import Config
 
 # the Strings used for this "thing"
 from translation import Translation
